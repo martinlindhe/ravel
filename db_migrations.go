a4e893ac964bc833ba5573e56df5df1f129b6ad3
Martin Lindhe <martin@ubique.se>
2015-12-16 23:12:10 +0100

    migrations wip

--- db_migrations.go
+++ db_migrations.go
@@ -2,41 +2,82 @@ package ravel
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
 
 	"github.com/jinzhu/gorm"
-	"github.com/martinlindhe/ravel/model"
+	"github.com/smallfish/simpleyaml"
 )
 
-func Migrate(db *gorm.DB) {
+func Migrate(db *gorm.DB) error {
 
 	fmt.Println("Running migrations ...")
 
 	db.Set("gorm:table_options", "ENGINE=InnoDB")
 
-	// XXX go thru a list of migrations and run them, somehow.. like in laravel
+	migrationsPath := "./db/migrations"
 
-	// XXX auto-register all found models
+	fileList, err := ioutil.ReadDir(migrationsPath)
+	if err != nil {
+		return err
+	}
 
-	user_create_Up(db)
+	for _, fi := range fileList {
+		ext := path.Ext(fi.Name())
+		if ext != ".yml" {
+			log.Printf("[migrate] Unrecognized extension %s, skipping %s\n", ext, fi.Name())
+			continue
+		}
 
-	// XXX can we introspect and find all structs in the model package, as strings?
-	//db.AutoMigrate(&model.User{})
+		fullPath := path.Join(migrationsPath, fi.Name())
 
-	// Feel free to change your struct, AutoMigrate will keep your database up-to-date.
-	// AutoMigrate will ONLY add *new columns* and *new indexes*,
-	// WON'T update current column's type or delete unused columns, to protect your data.
-	// If the table is not existing, AutoMigrate will create the table automatically.
+		err = runMigration(fullPath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	//	user_create_Up(db)
 
 	fmt.Println("Migrations successful!")
+	return nil
+}
+
+func runMigration(fileName string) error {
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	// read yml
+	fmt.Println(data)
+	yaml, err := simpleyaml.NewYaml(data)
+	if err != nil {
+		return err
+	}
+
+	upStep, err := yaml.Get("up").GetIndex(0).String()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(upStep)
+
+	// XXXX read up on gorm migration features....
+
+	return nil
 }
 
 func Rollback(db *gorm.DB) {
 	// XXX function that ROLLS BACK all migrations
 
 	fmt.Printf("Rolling back migrations... \n")
-	user_create_Down(db)
+	//user_create_Down(db)
 }
 
+/*
 func user_create_Up(db *gorm.DB) {
 	db.CreateTable(&model.User{})
 }
@@ -44,3 +85,4 @@ func user_create_Up(db *gorm.DB) {
 func user_create_Down(db *gorm.DB) {
 	db.DropTable(&model.User{})
 }
+*/
